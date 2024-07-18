a2903400cd4e605c787c9a66c6b9ee2e5ffbe1e3
nurrizkiamalia <rizkiamel9@gmail.com>
2024-07-18 12:34:30 +0700

    fix: my event list

--- app/dashboard/my-event/components/MyEventList.tsx
+++ app/dashboard/my-event/components/MyEventList.tsx
@@ -1,30 +1,14 @@
 "use client";
-
-import React, { useEffect, useRef, useCallback } from "react";
+import React, { useEffect } from "react";
 import MyEventCard from "./MyEventCard";
 import useEvent from "@/hooks/useEvent";
 
 const MyEventList: React.FC = () => {
-  const { events, fetchOrganizerEvents, loading, error, page, hasMore, setPage } = useEvent();
-  const observer = useRef<IntersectionObserver | null>(null);
-
-  const lastEventElementRef = useCallback(
-    (node: any) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPage(prevPage => prevPage + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [loading, hasMore, setPage]
-  );
+  const { events, fetchOrganizerEvents, loading, error } = useEvent();
 
   useEffect(() => {
-    fetchOrganizerEvents(page);
-  }, [fetchOrganizerEvents, page]);
+    fetchOrganizerEvents();
+  }, [fetchOrganizerEvents]);
 
   if (error) {
     return <p>{error}</p>;
@@ -32,15 +16,13 @@ const MyEventList: React.FC = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-      {events.map((event, index) => {
-        const key = `${event.id}-${index}`;
-        if (events.length === index + 1) {
-          return <MyEventCard ref={lastEventElementRef} key={key} event={event} index={index + 1} />;
-        } else {
-          return <MyEventCard key={key} event={event} index={index + 1} />;
-        }
-      })}
-      {loading && <p>Loading more events...</p>}
+      {events.map((event, index) => (
+        <MyEventCard key={`${event.id}-${index}`} event={event} index={index + 1} />
+      ))}
+      {loading && <p className="col-span-full text-center">Loading events...</p>}
+      {!loading && events.length === 0 && (
+        <p className="col-span-full text-center">No events found</p>
+      )}
     </div>
   );
 };
