ed13fb8c3f3b738588a67838815bcb4757a82dce
nurrizkiamalia <rizkiamel9@gmail.com>
2024-07-18 11:56:34 +0700

    fix: protected route in checkout

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -5,15 +5,13 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isAuthenticated, isLoading, currentUser } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
       router.push('/login');
-    } else if (currentUser?.role === "ORGANIZER") {
-      router.push('/dashboard/my-event');
-    } 
+    }
   }, [isLoading, isAuthenticated, router]);
 
   if (isLoading || !isAuthenticated) {
